675998ca0a529e82674d4def00ebafd70849ba4d
Miguel Mendiola Aragones <mmendiol@c1r10s1.42madrid.com>
2025-06-12 17:46:56 +0200

    casiiiiiiii

--- mendirueba/main.c
+++ mendirueba/main.c
@@ -1,13 +1,14 @@
-#include "./minilib/mlx.h"
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include "./minilibx/mlx.h"
+# include <math.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include <unistd.h>
 # include <stdarg.h>
 # include <string.h>
 # include <fcntl.h>
 # include <stdint.h>
 # include <limits.h>
+# include <sys/time.h>
 
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 720
@@ -35,7 +36,9 @@ typedef struct s_game
     double posX, posY;
     double dirX, dirY;
     double planeX, planeY;
-    int     color; 
+    int     color;
+    double		moveSpeed;
+    double		rotSpeed;
 }					t_game;
 
 
@@ -96,6 +99,28 @@ void	*ft_calloc(size_t count, size_t size)
 	return (str);
 }
 
+double obtenerTicks(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000.0) + (tv.tv_usec / 1000.0);
+}
+
+void print(double fps)
+{
+    printf("FPS: %.2f\n", fps);
+}
+
+void redibujado(t_game *game)
+{
+    mlx_do_sync(game->mlx);
+}
+
+void cls(t_game *game)
+{
+    mlx_clear_window(game->mlx, game->win);
+}
+
 // Dibuja una línea vertical
 void draw_vertical_line(t_game *game, int x, int drawStart, int drawEnd, int color)
 {
@@ -108,52 +133,140 @@ void draw_vertical_line(t_game *game, int x, int drawStart, int drawEnd, int col
 void draw_scene(t_game *game)
 {
     int x;
-    for (x = 0; x < SCREEN_WIDTH; x++)
+
+    game->posX = 22;
+    game->posY = 12;
+    game->dirX = -1;
+    game->dirY = 0;
+    game->planeX = 0;
+    game->planeY = 0.66;
+    
+    double time = 0; //tiempo del fotograma actual
+    double oldTime = 0; //tiempo del fotograma anterior
+
+
+    x = 0;
+    while (x < SCREEN_WIDTH)
     {
         double cameraX = 2 * x / (double)SCREEN_WIDTH - 1;
         double rayDirX = game->dirX + game->planeX * cameraX;
         double rayDirY = game->dirY + game->planeY * cameraX;
+        
+
+
+        
         int mapX = (int)(game->posX);
         int mapY = (int)(game->posY);
 
-        double sideDistX, sideDistY;
+        double sideDistX;
+        double sideDistY;
+        
         double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
         double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
         double perpWallDist;
         int stepX, stepY, hit = 0, side;
 
-        if (rayDirX < 0) { stepX = -1; sideDistX = (game->posX - mapX) * deltaDistX; }
-        else { stepX = 1; sideDistX = (mapX + 1.0 - game->posX) * deltaDistX; }
-        if (rayDirY < 0) { stepY = -1; sideDistY = (game->posY - mapY) * deltaDistY; }
-        else { stepY = 1; sideDistY = (mapY + 1.0 - game->posY) * deltaDistY; }
 
+
+        //Calcular el paso y la sideDist inicial
+        if (rayDirX < 0)
+        {
+            stepX = -1;
+            sideDistX = (game->posX - mapX) * deltaDistX;
+        }
+        else
+        {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - game->posX) * deltaDistX;
+        }
+        if (rayDirY < 0)
+        {
+            stepY = -1;
+            sideDistY = (game->posY - mapY) * deltaDistY;
+        }
+        else
+        {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - game->posY) * deltaDistY;
+        }
+
+
+        //realizar DDA
         while (hit == 0)
         {
-            if (sideDistX < sideDistY) { sideDistX += deltaDistX; mapX += stepX; side = 0; }
-            else { sideDistY += deltaDistY; mapY += stepY; side = 1; }
-            if (worldMap[mapX][mapY] > 0) hit = 1;
+            //salta al siguiente cuadrado del mapa, ya sea en la dirección x o en la dirección y
+            if (sideDistX < sideDistY)
+            {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            }
+            else
+            {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            //Comprueba si el rayo ha golpeado una pared
+            if (worldMap[mapX][mapY] > 0)
+                hit = 1;
         }
-        if (side == 0) perpWallDist = (mapX - game->posX + (1 - stepX) / 2) / rayDirX;
-        else perpWallDist = (mapY - game->posY + (1 - stepY) / 2) / rayDirY;
 
+
+
+        //Calcula la distancia proyectada en la dirección de la cámara (¡la distancia euclidiana produciría un efecto ojo de pez!)
+        if (side == 0)
+            perpWallDist = (mapX - game->posX + (1 - stepX) / 2) / rayDirX;
+        else
+            perpWallDist = (mapY - game->posY + (1 - stepY) / 2) / rayDirY;
+
+        //Calcula la altura de la línea a dibujar en la pantalla
         int lineHeight = (int)(SCREEN_HEIGHT / perpWallDist);
-        int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
-        if (drawStart < 0) drawStart = 0;
-        int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
-        if (drawEnd >= SCREEN_HEIGHT) drawEnd = SCREEN_HEIGHT - 1;
 
+        //Calcula el píxel más bajo y más alto para rellenar la franja actual
+        int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
+        if (drawStart < 0)
+            drawStart = 0;
+        int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+        if (drawEnd >= SCREEN_HEIGHT)
+            drawEnd = SCREEN_HEIGHT - 1;
+
+        //elige el color de la pared
         int color;
         switch (worldMap[mapX][mapY]) {
-            case 1:  color = 0xFF0000; break;
-            case 2:  color = 0x00FF00; break;
-            case 3:  color = 0x0000FF; break;
-            default: color = 0xAAAAAA; break;
+            case 1:
+                color = 0xFF0000;
+                break;
+            case 2:
+                color = 0x00FF00;
+                break;
+            case 3:
+                color = 0x0000FF;
+                break;
+            default:
+                color = 0xAAAAAA;
+                break;
         }
+        //dale a los lados x e y diferente brillo
         if (side == 1) color /= 2;
 
-        color = game->color; // <--- siempre el color global
+        //dibuja los píxeles de la raya como una línea vertical
         draw_vertical_line(game, x, drawStart, drawEnd, color);
+        x++;
     }
+
+    //sincronización de entrada y contador de FPS
+    oldTime = time;
+    time = obtenerTicks();
+    double frameTime = (time - oldTime) / 1000.0; //frameTime es el tiempo que ha tardado este fotograma, en segundos
+    print(1.0 / frameTime); //Contador de FPS
+    redibujado(game);
+    cls(game);
+
+    //modificadores de velocidad
+    game->moveSpeed = frameTime * 5.0; //el valor constante está en cuadrados/segundo
+    game->rotSpeed  = frameTime * 3.0; //el valor constante está en radianes/segundo
+
 }
 
 int	handle_key(int keycode, t_game *game)
@@ -165,21 +278,39 @@ int	handle_key(int keycode, t_game *game)
 	}
     if (keycode == KEY_W || keycode == KEY_UP)
     {
+        if(worldMap[(int)(game->posX + game->dirX * game->moveSpeed)][(int)(game->posY)] == 0) game->posX += game->dirX * game->moveSpeed;
+        if(worldMap[(int)(game->posX)][(int)(game->posY + game->dirY * game->moveSpeed)] == 0) game->posY += game->dirY * game->moveSpeed;
         printf("W");
         game->color = 0xFF0000;
     }
     if (keycode == KEY_S || keycode == KEY_DOWN)
     {
+        if(worldMap[(int)(game->posX - game->dirX * game->moveSpeed)][(int)(game->posY)] == 0) game->posX -= game->dirX * game->moveSpeed;
+        if(worldMap[(int)(game->posX)][(int)(game->posY - game->dirY * game->moveSpeed)] == 0) game->posY -= game->dirY * game->moveSpeed;
         printf("S");
         game->color = 0x0000FF;
     }
     if (keycode == KEY_A || keycode == KEY_LEFT)
     {
+        //tanto la dirección de la cámara como el plano de la cámara deben rotarse
+        double oldDirX = game->dirX;
+        game->dirX = game->dirX * cos(game->rotSpeed) - game->dirY * sin(game->rotSpeed);
+        game->dirY = oldDirX * sin(game->rotSpeed) + game->dirY * cos(game->rotSpeed);
+        double oldPlaneX = game->planeX;
+        game->planeX = game->planeX * cos(game->rotSpeed) - game->planeY * sin(game->rotSpeed);
+        game->planeY = oldPlaneX * sin(game->rotSpeed) + game->planeY * cos(game->rotSpeed);
         printf("A");
         game->color = 0xFFFF00;
     }
     if (keycode == KEY_D || keycode == KEY_RIGHT)
     {
+        //tanto la dirección de la cámara como el plano de la cámara deben rotarse
+        double oldDirX = game->dirX;
+        game->dirX = game->dirX * cos(-game->rotSpeed) - game->dirY * sin(-game->rotSpeed);
+        game->dirY = oldDirX * sin(-game->rotSpeed) + game->dirY * cos(-game->rotSpeed);
+        double oldPlaneX = game->planeX;
+        game->planeX = game->planeX * cos(-game->rotSpeed) - game->planeY * sin(-game->rotSpeed);
+        game->planeY = oldPlaneX * sin(-game->rotSpeed) + game->planeY * cos(-game->rotSpeed);
         printf("D");
         game->color = 0x00FF00;
     }
@@ -194,7 +325,9 @@ int	handle_cross(t_game *game)
 	exit(EXIT_SUCCESS);
 }
 
-int	main(int ac, char *av[])
+
+// gcc main.c ./minilibx/libmlx.a -lXext -lX11 -lm
+int main(int ac, char *argv[])
 {
 	t_game	*game;
 
@@ -203,6 +336,7 @@ int	main(int ac, char *av[])
         printf("MAL");
 	else
 	{
+
 		game->mlx = mlx_init();
         game->win = mlx_new_window(game->mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "Raycaster");
         
